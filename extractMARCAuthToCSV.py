0afcaa30be840cb01c3b5ad7e8106c6690e3715b
Michelle Janowiecki <mjanowi3@jh.edu>
2020-02-21 14:24:32 -0500

    Renaming and updating script with pandas

--- extractMARCAuthToCSV.py
+++ extractMARCAuthToCSV.py
@@ -1,7 +1,7 @@
 from pymarc import MARCReader
 import csv
 import argparse
-import re
+import pandas as pd
 import os
 from datetime import datetime
 
@@ -38,38 +38,51 @@ createDict(os.path.join(fileDir, 'dictionaries/marc_catsourceAuthorities.csv'), 
 
 #  Creates k,v pair in dict where key = field_name, value = values of MARC tags in record.
 def field_finder(record, field_name, tags):
-    field = record.get_fields(*tags)
     field_list = []
+    field = record.get_fields(*tags)
     for my_field in field:
         my_field = my_field.format_field()
         field_list.append(my_field)
-    mrc_fields[field_name] = field_list
+    if field_list:
+        field_list = '|'.join(str(e) for e in field_list)
+        mrc_fields[field_name] = field_list
+    else:
+        mrc_fields[field_name] = ''
 
 
 # Creates k,v pair in dict where key = field_name, value = values of specific subfield in MARC tag in record.
 def subfield_finder(record, field_name, subfields, tags):
-    field = record.get_fields(*tags)
     field_list = []
+    field = record.get_fields(*tags)
     for my_field in field:
-        my_field = my_field.get_subfields(*subfields)
-        for field in my_field:
+        my_subfield = my_field.get_subfields(*subfields)
+        for field in my_subfield:
             field_list.append(field)
-    mrc_fields[field_name] = field_list
+    if field_list:
+        field_list = '|'.join(str(e) for e in field_list)
+        mrc_fields[field_name] = field_list
+    else:
+        mrc_fields[field_name] = ''
 
 
 # Converts code from MARC record into name from imported dictionaries.
 def convert_to_name(keyname, dictname):
     for k, v in mrc_fields.items():
         if k == keyname:
-            for key, value in dictname.items():
-                if v == key:
-                    v = value
-                    mrc_fields[k] = v
+            if isinstance(v, list):
+                for count, item in enumerate(v):
+                    for key, value in dictname.items():
+                        if item == key:
+                            v[count] = value
+                mrc_fields[k] = v
+            else:
+                for key, value in dictname.items():
+                    if v == key:
+                        mrc_fields[k] = value
 
 
 all_fields = []
 record_count = 0
-
 with open(filename, 'rb') as fh:
     marc_recs = MARCReader(fh, to_unicode=True)
     for record in marc_recs:
@@ -87,20 +100,23 @@ with open(filename, 'rb') as fh:
         field_finder(record, '008', tags=['008'])
         field_finder(record, 'last_updated', tags=['005'])
         mrc_fields['type'] = [leader[6]]
-        keys = []
+
         # Edit & convert values in dictionary.
         for k, v in mrc_fields.items():
-            if k == '008':  # Find Lang codes, DtSt and Dates from field 008.
-                v = str(v[0])
-                type = v[9]
-                level = v[33]
-                cat_source = v[39]
+            # Find Lang codes, DtSt and Dates from field 008.
+            if k == '008':
+                if v:
+                    type = v[9]
+                    level = v[33]
+                    cat_source = v[39]
+                else:
+                    type = ''
+                    level = ''
+                    cat_source = ''
             elif k == 'last_updated':
-                v = str(v[0])
                 v = v[:4]
                 mrc_fields[k] = v
             elif k == 'number':
-                v = str(v[0])
                 v = v.replace(" ", "").strip()
                 mrc_fields[k] = v
 
@@ -112,29 +128,12 @@ with open(filename, 'rb') as fh:
         convert_to_name('level', level_dict)
         convert_to_name('cat_source', catsource_dict)
 
-        # Converts list values in k,v pair to strings joined by pipes.
-        for k, v in mrc_fields.items():
-            keys.append(k)
-            if isinstance(v, list):
-                if len(v) >= 2:
-                    v = "|".join(v)
-                    mrc_fields[k] = v
-                elif len(v) == 1:
-                    v = str(v[0])
-                    mrc_fields[k] = v
-                elif len(v) == 0:
-                    v = ''
-                    mrc_fields[k] = v
-                else:
-                    pass
         # Adds dict created by this MARC record to all_fields list.
         all_fields.append(mrc_fields)
         record_count = record_count + 1
         print(record_count)
 
+df = pd.DataFrame.from_dict(all_fields)
+print(df.head(15))
 dt = datetime.now().strftime('%Y-%m-%d %H.%M.%S')
-
-with open('marcAuth_'+dt+'.csv', 'w') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(all_fields)
+df.to_csv(path_or_buf='marcRecords_'+dt+'.csv', header='column_names', encoding='utf-8', sep=',', index=False)
